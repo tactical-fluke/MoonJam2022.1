dd0a1c55498d5235038801792c9790fd28b48138
Aidan Haile <tacticalfluke304@gmail.com>
2022-01-08 13:03:44 +0800

    Add tile aesthetic switching

--- Platform.gd
+++ Platform.gd
@@ -2,26 +2,35 @@ extends StaticBody2D
 
 export (int, 0, 1) var enabled_plane = 0
 
-export (bool) var switchable
+export (bool) var persistent
 
 var plane_manager
 
+export (Texture) var plane_1_texture
+export (Texture) var plane_2_texture
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if switchable:
-		plane_manager = get_node("/root/root/PlaneManager")
-		if plane_manager != null:
-			plane_manager.connect("switched_plane", self, "_on_plane_switched")
-		else:
-			print("Could not find the PlaneManager")
-			
-		_on_plane_switched(0)
+	plane_manager = get_node("/root/root/PlaneManager")
+	if plane_manager != null:
+		plane_manager.connect("switched_plane", self, "_on_plane_switched")
+	else:
+		print("Could not find the PlaneManager")
+	
+	_on_plane_switched(0)
+	$Sprite.texture = plane_1_texture
 
 
 func _on_plane_switched(plane):
-	if plane != enabled_plane:
-		$CollisionShape2D.call_deferred("set_disabled", true)
-		hide()
+	if !persistent:
+		if plane != enabled_plane:
+			$CollisionShape2D.call_deferred("set_disabled", true)
+			hide()
+		else:
+			$CollisionShape2D.call("set_disabled", false)
+			show()
 	else:
-		$CollisionShape2D.call("set_disabled", false)
-		show()
+		if plane == 0:
+			$Sprite.texture = plane_1_texture
+		else:
+			$Sprite.texture = plane_2_texture
